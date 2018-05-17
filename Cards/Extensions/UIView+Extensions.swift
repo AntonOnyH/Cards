b9181f7999fb2c6c942af42ebff8fb6aeaa8cdea
Hugo Prinsloo <hugo.prinsloo@icloud.com>
2018-05-17 12:50:37 +0200

    Added gradient extension and colors

---
+++ Cards/Extensions/UIView+Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  UIColor + Extensions.swift
+//  Cards
+//
+//  Created by Hugo Prinsloo on 2018/05/17.
+//  Copyright © 2018 Hugo. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func setGradientBackground(colorOne: UIColor, colorTwo: UIColor) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = [colorTwo.cgColor, colorOne.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 1.0, y: 1.0)
+        gradientLayer.endPoint = CGPoint(x: 0.0, y: 0.0)
+        
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
